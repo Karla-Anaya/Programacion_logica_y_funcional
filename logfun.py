3121dc9252b930c886569c8895a32bdaf15c27b0
Karla Anaya <stmalonsoso@gmail.com>
2020-06-16 21:18:30 -0500

    Ejercicio 1 logica funcional

--- logfun.py
+++ logfun.py
@@ -1,47 +1,46 @@
 Requisitos = {
-	"Oracle","SQL/PL","Linux","Unix","Shell","C++",
-	"Proc*C","TuxedoV12", "VB6", "Java","WebServices","MicroServicios"
+	"Oracle","SQL/PL","Linux","Unix","Shell","C++","Proc*C","TuxedoV12", "VB6", "Java","WebServices","MicroServicios"
 } 
 
-print("Responda con 1, si domina el campo")
+print("Responda con Si, si domina el campo")
 
 def entrevista():
-	R1 = input("\n Oracle BBDD : ")
-	R2 = input(" SQL y PL/SQL : ")
-	R3 = input(" Linux : ")
-	R4 = input(" Unix : ")
-	R5 = input(" Shell : ")
-	R6 = input(" C++ : ")
-	R7 = input(" Proc*C : ")
-	R8 = input(" TuxedoV12 : ")
-	R9 = input(" VB6 : ")
-	R10 = input(" Java : ")
-	R11 = input(" WebServices : ")
-	R12 = input(" MicroServicios : ")
+	R1 = input("\n Oracle BBDD : ").upper()
+	R2 = input(" SQL y PL/SQL : ").upper()
+	R3 = input(" Linux : ").upper()
+	R4 = input(" Unix : ").upper()
+	R5 = input(" Shell : ").upper()
+	R6 = input(" C++ : ").upper()
+	R7 = input(" Proc*C : ").upper()
+	R8 = input(" TuxedoV12 : ").upper()
+	R9 = input(" VB6 : ").upper()
+	R10 = input(" Java : ").upper()
+	R11 = input(" WebServices : ").upper()
+	R12 = input(" MicroServicios : ").upper()
 	Experiencia = set()
-	if R1 == "1":
+	if R1 == "SI":
 		Experiencia.add("Oracle")
-	if R2 == "1":
+	if R2 == "SI":
 		Experiencia.add("SQL/PL")
-	if R3 == "1":
+	if R3 == "SI":
 		Experiencia.add("Linux")
-	if R4 == "1":
+	if R4 == "SI":
 		Experiencia.add("Unix")
-	if R5 == "1":
+	if R5 == "SI":
 		Experiencia.add("Shell")
-	if R6 == "1":
+	if R6 == "SI":
 		Experiencia.add("C++")
-	if R7 == "1":
+	if R7 == "SI":
 		Experiencia.add("Proc*C")
-	if R8 == "1":
+	if R8 == "SI":
 		Experiencia.add("TuxedoV12")
-	if R9 == "1":
+	if R9 == "SI":
 		Experiencia.add("VB6")
-	if R10 == "1":
+	if R10 == "SI":
 		Experiencia.add("Java")
-	if R11 == "1":
+	if R11 == "SI":
 		Experiencia.add("WebServices")
-	if R12 == "1":
+	if R12 == "SI":
 		Experiencia.add("MicroServicios")
 	print ("Experiencia del usuario",Experiencia)
 	print ("Habilidades faltantes: ",Requisitos - Experiencia)
