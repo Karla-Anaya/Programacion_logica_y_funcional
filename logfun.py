bee5361e981936ee0b6e789e6dcba5113f8af83f
Karla Anaya <stmalonsoso@gmail.com>
2020-06-16 19:20:05 -0500

    Ejercicio 1 logica funcional

--- logfun.py
+++ logfun.py
@@ -1,19 +1,54 @@
-def entrevista(R):
-	if not R:
-		return []
-	if R:
-		print "Manejas: " , (R[0])
-		r = input()
-		if (r == 1):
-			return [R[0]] + entrevista(R[1:])
-		else:
-			return entrevista(R[1:])
+Requisitos = {
+	"Oracle","SQL/PL","Linux","Unix","Shell","C++",
+	"Proc*C","TuxedoV12", "VB6", "Java","WebServices","MicroServicios"
+} 
 
-Requisitos = {"Oracle","SQL/PL","Linux","Unix","Shell","C++","Proc*C","TuxedoV12", "VB6", "Java","WebServices","MicroServicios"}
-print "Responde con un 1 para si y 0 para no"
-L = list (Requisitos)
-R = set(entrevista(L))
-if (len(R) > 7 ):
-	print "Si estas capacitado para el trabajo"
-else:
-	print "No estas capacitado para el trabajo"
+print("Responda con 1, si domina el campo")
+
+def entrevista():
+	R1 = input("\n Oracle BBDD : ")
+	R2 = input(" SQL y PL/SQL : ")
+	R3 = input(" Linux : ")
+	R4 = input(" Unix : ")
+	R5 = input(" Shell : ")
+	R6 = input(" C++ : ")
+	R7 = input(" Proc*C : ")
+	R8 = input(" TuxedoV12 : ")
+	R9 = input(" VB6 : ")
+	R10 = input(" Java : ")
+	R11 = input(" WebServices : ")
+	R12 = input(" MicroServicios : ")
+	Experiencia = set()
+	if R1 == 1:
+		Experiencia.add("Oracle")
+	if R2 == 1:
+		Experiencia.add("SQL/PL")
+	if R3 == 1:
+		Experiencia.add("Linux")
+	if R4 == 1:
+		Experiencia.add("Unix")
+	if R5 == 1:
+		Experiencia.add("Shell")
+	if R6 == 1:
+		Experiencia.add("C++")
+	if R7 == 1:
+		Experiencia.add("Proc*C")
+	if R8 == 1:
+		Experiencia.add("TuxedoV12")
+	if R9 == 1:
+		Experiencia.add("VB6")
+	if R10 == 1:
+		Experiencia.add("Java")
+	if R11 == 1:
+		Experiencia.add("WebServices")
+	if R12 == 1:
+		Experiencia.add("MicroServicios")
+	print ("Experiencia del usuario",Experiencia)
+	print ("Habilidades faltantes: ",Requisitos - Experiencia)
+	if (len(Requisitos & Experiencia) >= 6):
+		return "SI ESTAS CAPACITADO PARA EL TRABAJO"
+	else:
+		return "NO ESTAS CAPACITADO PARA EL TRABAJO"
+
+print(entrevista())
+
